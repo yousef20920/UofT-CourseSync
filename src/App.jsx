ab8c3b319fc7ca2aa3797c6805aff91245ceeb57
Yousef Abdelhadi <mmax20920@gmail.com>
2024-08-21 02:33:19 -0400

    setting up

---
+++ src/App.jsx
@@ -0,0 +1,72 @@
+import { useState } from 'react';
+import { Container, Box } from '@chakra-ui/react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+import TextInput from './components/TextInput.jsx';
+import KeywordsModal from './components/KeywordsModal.jsx';
+
+const App = () => {
+  const [keywords, setKeywords] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const extractKeywords = async (text) => {
+    setLoading(true);
+    setIsOpen(true);
+
+    const options = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${import.meta.env.VITE_OPENAI_API_KEY}`,
+      },
+      body: JSON.stringify({
+        model: 'text-davinci-003',
+        prompt:
+          'Extract keywords from this text. Make the first letter of every word uppercase and separate with commas:\n\n' +
+          text +
+          '',
+        temperature: 0.5,
+        max_tokens: 60,
+        top_p: 1.0,
+        frequency_penalty: 0.8,
+        presence_penalty: 0.0,
+      }),
+    };
+
+    try {
+      const response = await fetch(
+        import.meta.env.VITE_OPENAI_API_URL,
+        options
+      );
+      const json = await response.json();
+      console.log(json.choices[0].text.trim());
+      setKeywords(json.choices[0].text.trim());
+      setLoading(false);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  return (
+    <Box bg='blue.600' color='white' height='100vh' paddingTop={130}>
+      <Container maxW='3xl' centerContent>
+        <Header />
+        <TextInput extractKeywords={extractKeywords} />
+        <Footer />
+      </Container>
+      <KeywordsModal
+        keywords={keywords}
+        loading={loading}
+        isOpen={isOpen}
+        closeModal={closeModal}
+      />
+    </Box>
+  );
+};
+
+export default App;
